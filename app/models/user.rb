e3e70c6c2d641274a41ba06a6782d79d0ae734bd
K93wyuta <k93wyuta@gmail.com>
2024-03-08 16:05:07 +0900

    ユーザー管理機能の実装_バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'は半角英字と半角数字それぞれ1文字以上含む必要があります'
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'は全角（カタカナ）で入力してください' }
+  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'は全角（カタカナ）で入力してください' }
   validates :birthday, presence: true
 end
