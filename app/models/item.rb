c04be7a46fefdf96b6ea5823b62695cc42a9f5d9
K93wyuta <k93wyuta@gmail.com>
2024-03-10 18:18:41 +0900

    モデルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,21 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one :record
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description,presence: true
+  validates :category_id, numericality: { only_integer: true, other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { only_integer: true, other_than: 1 , message: "can't be blank"}
+  validates :charge_id, numericality: { only_integer: true, other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { only_integer: true, other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id, numericality: { only_integer: true, other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, format: {with: /\A[a-z0-9]+\z/i} ,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+end
