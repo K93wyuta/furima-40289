b5765585f2f054b7a271817f9f92f658592289f0
K93wyuta <k93wyuta@gmail.com>
2024-03-11 09:57:29 +0900

    販売手数料と販売利益の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener("turbo:load", () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input",() => {
+    const inputValue = priceInput.value;
+    const fee = Math.floor(inputValue*0.1);
+    const profit = (inputValue - fee);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addProfit = document.getElementById("profit");
+    addTaxDom.innerHTML = `${fee}`;
+    addProfit.innerHTML = `${profit}`;
+  });
+})
