ae34104da2e85fe7e350d95edd0746b03c8c7753
K93wyuta <k93wyuta@gmail.com>
2024-03-13 19:23:04 +0900

    CBの修正

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city { '川崎市' }
     block { '宮前区' }
+    building {'テスト'}
     phone_number { '00000000000' }
     token { 'tok_abcdefghijk00000000000000000' }
   end
